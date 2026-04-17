8f09fcfb4f26a7351faf1d00624f322c64a8d677
navisjesley <navisjesley@gmail.com>
2026-04-17 13:24:07 +0100

    commit to test the acr build pipeline

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 
+// let's test the build and push image to acr github pipeline
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
